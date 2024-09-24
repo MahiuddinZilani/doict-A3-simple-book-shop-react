4a77838807b4ccf707ab5fde974a8006ef27dcbd
MahiuddinZilani <zilani.cse.cou@gmail.com>
2024-09-25 03:30:02 +0600

    book section is designed with slogan

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,7 +15,13 @@ function Home() {
   return (
     <>
       <Banner></Banner>
-      <div className=" mx-auto h-max container mt-8 grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      <hr />
+      <div className="mx-auto  container sticky top-[60px] z-10">
+        <h1 className="mx-auto mb-4 py-4 rounded-lg text-xl bg-gradient-to-l from-slate-900 to-orange-900 lg:text-3xl text-center text-white font-bold">
+          Explore Our Collection of Masterpieces
+        </h1>
+      </div>
+      <div className=" mx-auto h-max container grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {books.map((book) => (
           <BookCard key={book.id} book={book}></BookCard>
         ))}
