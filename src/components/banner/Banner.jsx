32c1677cea240bb382820f313deedcabb17ba34f
MahiuddinZilani <zilani.cse.cou@gmail.com>
2024-09-25 01:23:35 +0600

    banner section is modified

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,12 +2,12 @@ const Banner = () => {
   return (
     <div className="lg:flex flex-row lg:h-[24rem]">
       {/* large device */}
-      <div className="hidden lg:flex items-center justify-center w-full h-full bg-gradient-to-r from-slate-700 to-slate-400">
+      <div className="hidden lg:flex items-center justify-center w-full h-full bg-gradient-to-r from-slate-900 to-orange-900">
         <div className="text-center">
-          <h1 className="text-3xl font-semibold text-white">
+          <h1 className="text-2xl font-semibold text-white">
             Unlock a World of Stories <br /> Find Your Next Favorite Book at{" "}
             <br />
-            <span className="text-orange-500"> Book Store</span>
+            <span className="text-orange-400 text-4xl"> Book Store</span>
           </h1>
           <button className="w-auto px-5 py-2 mt-4 text-sm font-medium text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md  hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
             Buy Book
